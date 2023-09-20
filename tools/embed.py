8113f7527193babf5ebd836047f058e5f0f659a8
heapy <momonster1999@gmail.com>
2023-09-20 15:08:04 +0200

    Changed embed layout

--- tools/embed.py
+++ tools/embed.py
@@ -7,16 +7,16 @@ class Embed:
         team = tools.db.get_member_team(member, guild_id)
         
         if team == None:
-            embed = discord.Embed(color = discord.Colour.from_rgb(255, 0, 0), title = "User is not in any team", timestamp = datetime.utcnow())
+            embed = discord.Embed(color = discord.Colour.from_rgb(255, 0, 0), title = "User not found in Database", timestamp = datetime.utcnow())
             embed.set_footer(text = "Turniejowy by heapy")
             return embed
             
         embed = discord.Embed(color = discord.Colour.from_rgb(255, 255, 255), timestamp = datetime.utcnow())  
 
         embed.set_author(name = member.name, icon_url = member.avatar.url)
-        embed.add_field(name = "Team", value = team.name, inline = False)
+        embed.add_field(name = "Team Name", value = team.name, inline = False)
         embed.add_field(name = "Team Owner", value = f"<@{team.owner_id}>", inline = False)
-        embed.add_field(name = "Team Members", value = "\n".join([f"<@{x.id}>" if x.id != team.owner_id else None for x in team.members]), inline = False)
+        embed.add_field(name = "Other Team Members", value = "\n".join([f"<@{x.id}>" if x.id != team.owner_id else None for x in team.members]), inline = False)
 
         embed.set_footer(text = "Turniejowy by heapy")
         
