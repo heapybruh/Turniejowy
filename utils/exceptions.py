141b80e22a1122a1d611f5185989c527236fa13d
heapy <momonster1999@gmail.com>
2023-10-09 15:55:18 +0200

    Updated exceptions

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,11 +1,11 @@
 class NoConfigException(Exception):
     def __init__(self):
-        self.message = "Config file does not exist"
+        self.message = "Config file does not exist."
         super().__init__(self.message)
         
 class WrongConfigException(Exception):
     def __init__(self):
-        self.message = "Config file is wrong, one of required keys is not in the config"
+        self.message = "Config file is wrong, one of required keys is not in the config."
         super().__init__(self.message)
         
 class NoAdminException(Exception):
@@ -16,4 +16,9 @@ class NoAdminException(Exception):
 class TooSmallTeamException(Exception):
     def __init__(self):
         self.message = "The team is too small! Minimum size is 2-stacked team."
+        super().__init__(self.message)
+        
+class TeamNotFoundException(Exception):
+    def __init__(self):
+        self.message = "Team not found!"
         super().__init__(self.message)
